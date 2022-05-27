0ad000a5ac4037654657c1365b1d8d4f7a699646
christojo7n <106387776+christojo7n@users.noreply.github.com>
2022-05-27 19:13:41 +0530

    Delete Land_Plot directory

--- Land_Plot/App/db_connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$con = mysqli_connect('localhost', 'root', '', 'land_plot');
-	if(!$con)
-		die("ERROR: Couldn't connect to database");
-?>
--- Land_Plot/python/test.py
+++
@@ -1,6 +0,0 @@
-#! C:\Users\7ohn\AppData\Local\Programs\Python\Python39\python.exe
-import time
-print("Content-Type: text/html\n")
-
-for i in range(1000):
-    print(i,"This is python")
